2664a1a8940ef62049a0983c0fcd3327c9a7830c
Денис Бут <58259559+Onee-Cloud@users.noreply.github.com>
2024-04-27 14:10:22 +0300

    Added settings

--- main.py
+++ main.py
@@ -1,32 +1,86 @@
 import curses
 from curses import wrapper
+from settings import *
 
-world_map = []
 
-class Player:
-    def __init__(self, speed, x, y):
+
+
+class World:
+
+    def __init__(self, stdscr, map_path, world_map):
+        self.stdscr = stdscr
+        self.map_path = map_path
+        self.world_map = world_map
+
+    def read_map(self):
+        try:
+            with open(self.map_path, 'r') as file:
+                for line in file:
+                    row = []
+                    for char in line.strip():
+                        row.append(char)
+                    self.world_map.append(row)
+        except FileNotFoundError:
+            print(f"Файл {self.map_path} не знайдено.")
+        except Exception as e:
+            print(f"Помилка: {e}")
+    
+    def save_map(self):
+        try:
+            with open(self.map_path, 'w') as file:
+                for row in self.world_map:
+                    file.write(''.join(row) + '\n')
+            print(f"Дані збережено у файлі {self.map_path}")
+        except Exception as e:
+            print(f"Помилка при збереженні даних у файл: {e}")
+
+    def draw_map(self):
+        for y in range(len(self.world_map)):
+            for x in range(len(self.world_map[y])):
+                self.stdscr.addstr(y,x, str(self.world_map[y][x]), curses.color_pair(int(self.world_map[y][x])))
+    
+class Player(World):
+
+    def __init__(self, stdscr, player_path, speed, x, y):
         self.speed = speed
+        self.player_path = player_path
         self.x = x
         self.y = y
+        self.stdscr = stdscr
     
     def read_pos(self):
         try:
-            with open("player.txt", 'r') as file:
+            with open(self.player_path, 'r') as file:
                 position = file.readline().strip().split(' ')
                 self.x = int(position[0])
                 self.y = int(position[1])
         except FileNotFoundError:
-            print(f"Файл player.txt не знайдено.")
+            print(f"Файл {self.player_path} не знайдено.")
         except Exception as e:
             print(f"Помилка: {e}")
+    
+    def save_pos(self):
+        try:
+            with open(self.player_path, 'w') as file:
+                file.write(f"{self.x} {self.y}")
+            print(f"Позицію гравця збережено у файлі {self.player_path}")
+        except Exception as e:
+            print(f"Помилка при збереженні позиції гравця у файл: {e}")
 
-def read_map():
-    with open("map.txt", 'r') as file:
-        for line in file:
-            row = []
-            for char in line.strip():
-                row.append(char)
-            world_map.append(row)
+    def set_player_pos(self, x, y):
+        self.x = x
+        self.y = y
+        self.stdscr.addstr(self.x, self.y, '@', curses.color_pair(5))
+    
+    def move(self, dir):
+        if dir == "UP":
+            print("Move UP")
+        elif dir == "DOWN":
+            pass
+        elif dir == "LEFT":
+            pass
+        elif dir == "RIGHT":
+            pass
 
 def init_colors(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_GREEN)#Трава
@@ -35,26 +89,38 @@ def init_colors(stdscr):
     curses.init_pair(4, curses.COLOR_RED, curses.COLOR_RED)#Ворог
     curses.init_pair(5, curses.COLOR_WHITE, curses.COLOR_WHITE)#Гравець
 
-def draw_map(stdscr):
-    for y in range(len(world_map)):
-        for x in range(len(world_map[y])):
-            stdscr.addstr(y,x, str(world_map[y][x]), curses.color_pair(int(world_map[y][x])))
-
 def main(stdscr):
-    init_colors(stdscr)
-    read_map()
-    stdscr.clear()
-    draw_map(stdscr)
-    stdscr.refresh()
-    
-    player = Player(speed=1,x=1,y=1)
-    player.read_pos()
-    stdscr.addstr(player.x, player.y, '@', curses.color_pair(5))
+
     curses.curs_set(0)
+    stdscr.clear()
+    init_colors(stdscr)
+    world = World(stdscr, map_path="map.txt", world_map=[])
+    world.read_map()
+    world.draw_map()
+    stdscr.refresh()
+    player = Player(stdscr, player_path="player.txt", speed=1, x=1, y=1)
+    player.read_pos()
+    player.set_player_pos(player.x, player.y)
+
+    
     while True:
         key = stdscr.getch()
-        if key == ord('q') or key == ord('Q'):
+        if key in QUIT:
+            world.save_map()
+            player.save_pos()
             break
+
+        if key in UP:
+            player.move("UP")
+        
+        if key in DOWN:
+            player.move("DOWN")
+        
+        if key in LEFT:
+            player.move("LEFT")
+        
+        if key in RIGHT:
+            player.move("RIGHT")
         
 
 wrapper(main)
---
+++ settings.py
@@ -0,0 +1,5 @@
+UP = [ ord('w'),  ord('W'),  ord('ц'),  ord('Ц'), 259]
+DOWN = [ ord('s'),  ord('S'),  ord('і'),  ord('І'), 258]
+LEFT = [ ord('a'),  ord('A'),  ord('ф'),  ord('Ф'), 260]
+RIGHT = [ ord('d'),  ord('D'),  ord('в'),  ord('В'), 261]
+QUIT = [ ord('q'),  ord('Q'),  ord('й'),  ord('Й')]
